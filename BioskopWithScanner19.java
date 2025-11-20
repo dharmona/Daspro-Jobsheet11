659e9b350b02e2388f529d5d628e7b5ab0bd8fc2
panntod <munjalindra.pandhu@gmail.com>
2025-11-20 16:30:36 +0700

    Add implementation of BioskopWithScanner19 program with menu options for inputting and displaying audience data

---
+++ BioskopWithScanner19.java
@@ -0,0 +1,90 @@
+import java.util.Scanner;
+
+public class BioskopWithScanner19 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int baris, kolom, menu, jarakBaris = 4;
+        String nama, next, penonton[][] = new String[4][2];
+
+        while (true) {
+            System.out.println("Selamat Datang!  ");
+            System.out.println("[1] Input data Penonton\n[2] Tampilkan daftar penonton\n[3] Exit");
+            System.out.print("Silahkan pilih menu:  ");
+            menu = sc.nextInt();
+            sc.nextLine();
+
+            if (menu == 1) {
+                while (true) {
+                    System.out.println("=".repeat(50));
+                    System.out.print("Masukkan nama: ");
+                    nama = sc.nextLine();
+                    System.out.print("Masukkan baris (maks: " + penonton.length + ") : ");
+                    baris = sc.nextInt();
+                    System.out.print("Masukkan kolom (maks: " + penonton[0].length + ") : ");
+                    kolom = sc.nextInt();
+                    sc.nextLine();
+                    if(baris > penonton.length || kolom > penonton[0].length){
+                        System.out.println("Kursi tidak tersedia, silahkan inputkan ulang!");
+                        continue;
+                    }
+                    if(penonton[baris-1][kolom-1] != null){
+                        System.out.println("Kursi telah dipesan, silahkan pilih kursi yang lain!");
+                        continue;
+                    }
+                    penonton[baris-1][kolom-1] = nama;
+                    
+                    System.out.print("Input penonton lainnya? (y/n): ");
+                    next = sc.nextLine();
+                    
+                    if (next.equalsIgnoreCase("n")) {
+                        System.out.println("=".repeat(50));
+                        break;
+                    }
+                }
+            } else if (menu == 2) {
+                //
+                // NYOBA NGULIK BUAT TAMPILAN SEPERTI TABEL IBU🙏
+                // MOHON MAAF KALAU BERANTAKAN KODE NYA IBU
+                //
+                System.out.println("=".repeat(50));
+                System.out.println("Daftar Penonton:\n");
+                for (int i = 0; i < penonton.length; i++) {
+                    for (int j = 0; j < penonton[i].length; j++) {
+                        if (penonton[i][j] != null) {
+                            if (penonton[i][j].length() > jarakBaris) {
+                                jarakBaris = penonton[i][j].length();
+                            }
+                        }
+                    }
+                }
+                for (int i = 0; i < penonton[0].length; i++) {
+                    System.out.printf("%"+ (jarakBaris/2+2) +"s %d %" + (jarakBaris/4) + "s", "", (i + 1), "");
+                }
+                System.out.println();
+                for (int i = 0; i < penonton.length; i++) {
+                    System.out.println("-".repeat((jarakBaris * penonton[0].length) + 8));
+                    System.out.print((i + 1) + "| ");
+                    for (int j = 0; j < penonton[i].length; j++) {
+                        //TAMPILAN ISI ARRAY DISINI
+                        System.out.printf("%-" + jarakBaris +"s | ", penonton[i][j] != null ? penonton[i][j] : "***");
+                    }
+                    System.out.println();
+                }
+                System.out.println("-".repeat((jarakBaris * penonton[0].length) + 8 ) + "\n");
+                System.out.println("=".repeat(50));
+            } else if (menu == 3) {
+                break;
+            } else{
+                System.out.println("-".repeat(50));
+                System.out.println("Pilihan tidak valid! Silahkan ulangi");
+                System.out.println("-".repeat(50));
+                continue;
+            }
+
+        }
+        sc.close();
+
+
+
+    }
+}
---
+++ BioskopWithScanner19.md
@@ -0,0 +1,15 @@
+
+1.  Apakah pengisian elemen array dari scanner harus dilakukan secara berurutan mulai dari indeks ke-0 Jelaskan! 
+2.  Modifikasi kode program untuk memberikan opsi menu sebagai berikut: 
+    - Menu 1: Input data penonton 
+    - Menu 2: Tampilkan daftar penonton 
+    - Menu 3: Exit 
+3.  Modifikasi kode program untuk menghandle apabila nomor baris/kolom kursi yang tidak tersedia 
+4.  Pada menu 1, modifikasi kode program untuk memberikan warning apabila kursi yang dipilih sudah terisi oleh penonton lainnya lalu munculkan perintah untuk memasukkan baris dan kolom kembali  
+
+Jawaban:
+
+1.  Jawabannya tidak harus, pengisian elemen array dapat dilakukan dari indeks ke berapapun, namun jika pengisiannya tidak berurutan atau dilewati, maka elemen sebelumnya akan bernilai default sesuai tipe data nya.
+2. DONE
+3. DONE
+4. DONE
